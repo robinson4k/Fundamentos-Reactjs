6a4b1e16e82e797410bd4cf9759d7d5ef0103fc4
Robinson Silva <robinson4k@gmail.com>
2024-04-06 01:17:35 -0400

    Estado (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,8 +4,15 @@ import { ptBR } from "date-fns/locale/pt-BR";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
+import { useState } from "react";
 
 export function Post({ author, publishedAt, content }) {
+
+  // estado = variaveis que eu quero que o componente monitore
+  const [comments, setComments] = useState([
+    1, 2
+  ])
+
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -18,6 +25,12 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1])
+  }
 
   return (
     <article className={styles.post}>
@@ -45,7 +58,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe seu comentario" />
         <footer>
@@ -54,7 +67,11 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
+        {comments.map(comment => {
+          return (
+            <Comment />
+          )
+        })}
       </div>
     </article>
   );
